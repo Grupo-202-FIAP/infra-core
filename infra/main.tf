b175388d337becc9f70dfcc067404705e2a54cc1
Jefferson Araujo Silva <111441175+Jefferson-Araujo-Silva@users.noreply.github.com>
2025-10-06 20:30:33 -0300

    Dev (#8)
    * feat: infra inicial e ci cd
    
    * removendo comentario
    
    * test ci cd
    
    * ajust to format dont impact the deploy
    
    * feat: infra-core
    
    * add pull request (#4)
    
    * Feature/infra inicial (#5)
    
    * feat: add API Gateway module with integration and outputs
    
    * feat: rds
    
    * add s3 state
    
    * fix: commit para o leo
    
    * ajuste rds e s3
    
    * fix: finalizando subida s3
    
    * fix: finalizando subida s3
    
    * fix: commit para o leo
    
    * ajuste dbSubnetGroup
    
    ---------
    
    Co-authored-by: fernandeeess <matheusfernandesrodrigues@outlook.com>
    Co-authored-by: leobr1t0 <leonardo.oliveira@sptech.school>
    Co-authored-by: MarceloACJunior <marcelinhoalcantaracruz@gmail.com>
    
    * Feature/infra inicial (#7)
    
    * feat: add API Gateway module with integration and outputs
    
    * feat: rds
    
    * add s3 state
    
    * fix: commit para o leo
    
    * ajuste rds e s3
    
    * fix: finalizando subida s3
    
    * fix: finalizando subida s3
    
    * fix: commit para o leo
    
    * ajuste dbSubnetGroup
    
    * fix: descomentando s3
    
    ---------
    
    Co-authored-by: fernandeeess <matheusfernandesrodrigues@outlook.com>
    Co-authored-by: leobr1t0 <leonardo.oliveira@sptech.school>
    Co-authored-by: MarceloACJunior <marcelinhoalcantaracruz@gmail.com>
    
    ---------
    
    Co-authored-by: leobr1t0 <leonardo.oliveira@sptech.school>
    Co-authored-by: Leonardo Brito de Oliveira <111136765+leobr1t0@users.noreply.github.com>
    Co-authored-by: Erick Abner <erickabner51@gmail.com>
    Co-authored-by: fernandeeess <matheusfernandesrodrigues@outlook.com>
    Co-authored-by: MarceloACJunior <marcelinhoalcantaracruz@gmail.com>

---
+++ infra/main.tf
@@ -0,0 +1,102 @@
+module "vpc" {
+  source     = "./modules/vpc"
+  vpc_name   = var.vpc_name
+  cidr_block = var.cidr_block
+  tags       = var.tags
+}
+
+module "private_subnet" {
+  source            = "./modules/subnet/private_subnet"
+  subnet_name       = var.subnet_name
+  vpc_id            = module.vpc.vpc_id
+  azs               = var.azs
+  private_subnets   = var.private_subnets
+  subnet_group_name = var.subnet_group_name
+  tags              = var.tags
+}
+
+module "public_subnet" {
+  source         = "./modules/subnet/public_subnet"
+  subnet_name    = var.subnet_name
+  vpc_id         = module.vpc.vpc_id
+  azs            = var.azs
+  public_subnets = var.public_subnets
+  tags           = var.tags
+}
+
+module "internet_gateway" {
+  source   = "./modules/internet_gateway"
+  igw_name = var.igw_name
+  vpc_id   = module.vpc.vpc_id
+  tags     = var.tags
+}
+
+module "nat_gateway" {
+  source           = "./modules/nat_gateway"
+  nat_name         = var.nat_name
+  public_subnet_id = module.public_subnet.public_subnet_ids[0]
+  tags             = var.tags
+}
+
+module "route_table" {
+  source           = "./modules/route_table"
+  route_table_name = var.route_table_name
+  vpc_id           = module.vpc.vpc_id
+  subnet_ids       = module.public_subnet.public_subnet_ids
+  gateway_id       = module.internet_gateway.igw_id
+  route_cidr       = var.route_cidr
+  tags             = var.tags
+}
+
+module "security_group_api" {
+  source      = "./modules/security_group/public_sg"
+  sg_api_name = var.sg_api_name
+  vpc_id      = module.vpc.vpc_id
+  tags        = var.tags
+}
+
+module "security_group_postgres" {
+  source           = "./modules/security_group/private_sg"
+  sg_postgres_name = var.sg_postgres_name
+  vpc_id           = module.vpc.vpc_id
+  api_sg_id        = module.security_group_api.security_group_id
+  tags             = var.tags
+}
+
+# 🔹 ACL
+module "acl" {
+  source    = "./modules/acl"
+  acl_name  = var.acl_name
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.public_subnet.public_subnet_ids[0]
+  tags      = var.tags
+}
+
+module "api_gateway" {
+  source      = "./modules/api_gateway"
+  name        = var.api_gw_name
+  description = var.api_gw_description
+  root_path   = var.api_gw_root_path
+  stage_name  = var.api_stage_name
+}
+
+module "rds_instance" {
+  source               = "./modules/rds"
+  rds_identifier_name  = var.rds_identifier_name
+  rds_sg_ids           = [module.security_group_postgres.postgres_sg_id]
+  db_subnet_group_name = module.private_subnet.subnet_group_name
+  private_subnet_ids   = module.private_subnet.private_subnet_ids
+
+  rds_username_secret_name = var.rds_username_ssm_path
+  rds_password_secret_name = var.rds_password_ssm_path
+
+  instance_class    = var.instance_class
+  allocated_storage = var.allocated_storage
+  engine            = var.engine
+  engine_version    = var.engine_version
+}
+
+module "s3" {
+  source      = "./modules/s3"
+  bucket_name = var.bucket_name
+}
---
+++ infra/variables.tf
@@ -0,0 +1,142 @@
+variable "vpc_name" {
+  type        = string
+  description = "Nome da VPC"
+  default     = ""
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Prefixo para nome das subnets"
+}
+
+variable "nat_name" {
+  type        = string
+  description = "Nome do Nat Gateway"
+}
+
+variable "subnet_group_name" {
+  type        = string
+  description = "Nome para o grupo das subnets"
+}
+
+variable "igw_name" {
+  type        = string
+  description = "Nome do Internet Gateway"
+}
+
+variable "sg_api_name" {
+  type        = string
+  description = "Nome do Security Group da API"
+}
+
+variable "sg_postgres_name" {
+  type        = string
+  description = "Nome do Security Group do PostgreSQL"
+}
+
+variable "acl_name" {
+  type        = string
+  description = "Nome do Network ACL"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "CIDR da VPC"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags aplicadas aos recursos"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Zona de disponibilidade"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "CIDR das subnets públicas"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "CIDR da subnet privada"
+}
+
+variable "route_cidr" {
+  type        = string
+  default     = "0.0.0.0/0"
+  description = "CIDR para rota padrão"
+}
+
+variable "rds_username_ssm_path" {
+  description = "Caminho (name) no SSM Parameter Store para o nome de usuário do RDS."
+  type        = string
+}
+
+variable "rds_password_ssm_path" {
+  description = "Caminho (name) no SSM Parameter Store para a senha do RDS."
+  type        = string
+}
+
+variable "instance_class" {
+  description = "Classe da instância RDS (ex: db.t3.micro, db.m6g.large)."
+  type        = string
+}
+
+variable "allocated_storage" {
+  description = "Tamanho inicial do armazenamento alocado para a instância RDS em GB."
+  type        = number
+}
+
+variable "engine" {
+  description = "Engine do banco de dados (ex: postgres, mysql, oracle-se2)."
+  type        = string
+}
+
+variable "engine_version" {
+  description = "Versão principal do Engine do banco de dados (ex: 15, 14.7)."
+  type        = string
+}
+
+variable "api_gw_name" {
+  description = "Nome da API REST no API Gateway."
+  type        = string
+}
+
+variable "api_gw_description" {
+  description = "Descrição da API REST no API Gateway."
+  type        = string
+}
+
+variable "api_gw_root_path" {
+  description = "Nome do recurso raiz (root resource) para a API (ex: items, products)."
+  type        = string
+}
+
+variable "api_stage_name" {
+  description = "Nome do stage de deploy da API (ex: dev, hom, prod)."
+  type        = string
+}
+
+variable "rds_identifier_name" {
+  description = "Identificador único da instância RDS (o nome da instância na AWS)."
+  type        = string
+}
+
+variable "route_table_name" {
+  description = "Nome do recurso para a Tabela de Rotas Pública (ex: infra-public-rt)."
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Nome do bucket"
+  type        = string
+}
+
+variable "region" {
+  description = "Região da AWS"
+  type = string
+}
