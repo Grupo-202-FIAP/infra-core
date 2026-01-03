77a8052ae4eadff77d48adb099b2f3de51246608
leobr1t0 <leonardo70177@gmail.com>
2026-01-02 23:14:19 -0300

    fix: rename bucket

--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -1,9 +1,11 @@
 variable "bucket_name" {
   description = "Nome do bucket S3 para armazenar o state"
   type        = string
+  default     = "nextime-food-state-bucket"
 }
 
 variable "environment" {
   description = "Ambiente de deploy"
   type        = string
+  default     = "dev"
 }
