e74b70330eda574782c0deeb5932df7c8085b887
fernandeeess <matheusfernandesrodrigues@outlook.com>
2026-01-07 15:26:06 -0300

    Refactor S3 bucket configuration: uncomment and clean up code, remove redundant outputs

--- infra/bootstrap/outputs.tf
+++ infra/bootstrap/outputs.tf
@@ -1,4 +1,4 @@
-```terraform
+ 
 output "s3_bucket_name" {
 	description = "Nome do bucket S3 usado como backend"
 	value       = try(aws_s3_bucket.terraform_state[0].bucket, "")
@@ -8,13 +8,4 @@ output "s3_bucket_arn" {
 	description = "ARN do bucket S3 usado como backend"
 	value       = try(aws_s3_bucket.terraform_state[0].arn, "")
 }
-```
-output "s3_bucket_name" {
-	description = "Nome do bucket S3 usado como backend"
-	value       = try(aws_s3_bucket.terraform_state.bucket, "")
-}
-
-output "s3_bucket_arn" {
-	description = "ARN do bucket S3 usado como backend"
-	value       = try(aws_s3_bucket.terraform_state.arn, "")
-}
+ 
