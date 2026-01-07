77fa50aa3c2146cb134eebb988fa02bc5a66863e
fernandeeess <matheusfernandesrodrigues@outlook.com>
2026-01-07 15:18:44 -0300

    Add conditional S3 bucket creation and update outputs for safety

--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -1,3 +1,23 @@
+```terraform
+variable "bucket_name" {
+  description = "Nome do bucket S3 para armazenar o state"
+  type        = string
+  default     = "nextime-food-state-bucket"
+}
+
+variable "enable_s3" {
+  description = "Habilita criação do bucket S3 (usar false para desativar deploy S3)"
+  type        = bool
+  default     = false
+}
+
+variable "environment" {
+  description = "Ambiente de deploy"
+  type        = string
+  default     = "dev"
+}
+
+```
 # variable "bucket_name" {
 #   description = "Nome do bucket S3 para armazenar o state"
 #   type        = string
