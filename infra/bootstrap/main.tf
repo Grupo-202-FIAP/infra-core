d18237de482cf1663c9b6700728c44a3c72a1e3e
fernandeeess <matheusfernandesrodrigues@outlook.com>
2026-01-07 11:43:20 -0300

    Comment out S3 bucket configuration and related variables

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -1,30 +1,30 @@
 # Criação do bucket S3
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = var.bucket_name
 
-  tags = {
-    Name        = "terraform-state"
-    Environment = var.environment
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   tags = {
+#     Name        = "terraform-state"
+#     Environment = var.environment
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+# resource "aws_s3_bucket_versioning" "versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+#   bucket = aws_s3_bucket.terraform_state.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -1,8 +1,8 @@
-variable "bucket_name" {
-  description = "Nome do bucket S3 para armazenar o state"
-  type        = string
-  default     = "nextime-food-state-bucket"
-}
+# variable "bucket_name" {
+#   description = "Nome do bucket S3 para armazenar o state"
+#   type        = string
+#   default     = "nextime-food-state-bucket"
+# }
 
 variable "environment" {
   description = "Ambiente de deploy"
