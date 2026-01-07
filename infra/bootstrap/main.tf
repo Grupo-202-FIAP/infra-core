4b1bd7db04d34e5ee2e0eb0be3bc1098a4fbafff
Matheus Fernandes Rodrigues <111136519+Fernandeess@users.noreply.github.com>
2026-01-07 15:22:22 -0300

    Update vpc (#33)
    * Add RDS DB Subnet Group resource and output
    
    * Add lifecycle block to prevent destruction of S3 bucket
    
    * Add private route table module and associated resources
    
    * Add VPC endpoint and security group for Cognito integration
    
    * disable s3 creaion
    
    * Comment out S3 bucket configuration and related variables
    
    * Add conditional S3 bucket creation and update outputs for safety
    
    * Fix output definitions for S3 bucket name and ARN to use correct indexing
    
    ---------
    
    Co-authored-by: fernandeeess <matheusfernandesrodrigues@outlook.com>

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -22,9 +22,39 @@
 # resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 #   bucket = aws_s3_bucket.terraform_state.id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+ 
+# Criação do bucket S3 (condicional)
+resource "aws_s3_bucket" "terraform_state" {
+	count  = var.enable_s3 ? 1 : 0
+	bucket = var.bucket_name
+
+	tags = {
+		Name        = "terraform-state"
+		Environment = var.environment
+	}
+	lifecycle {
+		prevent_destroy = true
+	}
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+	count  = var.enable_s3 ? 1 : 0
+	bucket = aws_s3_bucket.terraform_state[0].id
+
+	versioning_configuration {
+		status = "Enabled"
+	}
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+	count  = var.enable_s3 ? 1 : 0
+	bucket = aws_s3_bucket.terraform_state[0].id
+
+	rule {
+		apply_server_side_encryption_by_default {
+			sse_algorithm = "AES256"
+		}
+	}
+}
+
+ 
--- infra/bootstrap/outputs.tf
+++ infra/bootstrap/outputs.tf
@@ -1,9 +1,20 @@
+```terraform
 output "s3_bucket_name" {
-  description = "Nome do bucket S3 usado como backend"
-  value       = aws_s3_bucket.terraform_state.bucket
+	description = "Nome do bucket S3 usado como backend"
+	value       = try(aws_s3_bucket.terraform_state[0].bucket, "")
 }
 
 output "s3_bucket_arn" {
-  description = "ARN do bucket S3 usado como backend"
-  value       = aws_s3_bucket.terraform_state.arn
+	description = "ARN do bucket S3 usado como backend"
+	value       = try(aws_s3_bucket.terraform_state[0].arn, "")
+}
+```
+output "s3_bucket_name" {
+	description = "Nome do bucket S3 usado como backend"
+	value       = try(aws_s3_bucket.terraform_state.bucket, "")
+}
+
+output "s3_bucket_arn" {
+	description = "ARN do bucket S3 usado como backend"
+	value       = try(aws_s3_bucket.terraform_state.arn, "")
 }
