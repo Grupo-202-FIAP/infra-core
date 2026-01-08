5aa945009cdbdc130c86317f7f4d20b75b3dfe14
MarceloACJunior <marcelinhoalcantaracruz@gmail.com>
2026-01-07 22:38:43 -0300

    fix: terrafom infra

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,14 +5,13 @@ terraform {
       version = "6.14.1"
     }
   }
- /*
-  backend "s3" {
-    bucket  = "nextime-food-state-bucket"
-    key     = "infra-core/infra.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-  */
+  
+  # backend "s3" {
+  #   bucket  = "nextime-food-state-bucket"
+  #   key     = "infra-core/infra.tfstate"
+  #   region  = "us-east-1"
+  #   encrypt = true
+  # }
  
 }
 
